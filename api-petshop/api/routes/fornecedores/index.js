9ce2832aeefe07c8295443499de47ddd43a8406f
estevaoreis25 <estevaoreis1587@gmail.com>
2021-06-22 15:36:40 -0300

    Impementation os HTTP status

--- api-petshop/api/routes/fornecedores/index.js
+++ api-petshop/api/routes/fornecedores/index.js
@@ -2,22 +2,29 @@ const roteador = require('express').Router()
 const TabelaFornecedor = require('./TabelaFornecedor')
 const Fornecedor = require('./Fornecedor')
 
+
+// View All Providers
 roteador.get('/', async (req, res) => {
-  const resultados = await TabelaFornecedor.listar()
+  const resultados = await TabelaFornecedor.listar();
+  res.status(200);
   res.send(JSON.stringify(resultados));
 })
 
+// Create a Providers
 roteador.post('/', async (req, res) => {
   try{
     const dados = req.body;
     const fornecedor = new Fornecedor(dados);
     await fornecedor.criar();
-    res.send(fornecedor)
+    res.status(201);
+    res.send(fornecedor);
   } catch(err){
+    res.status(400);
     res.send(JSON.stringify({msg:err.message}));
   }
 })
 
+// Search a Provider
 roteador.get('/:idFornecedor', async (req, res) => {
   try{
     const {idFornecedor} = req.params;
@@ -25,8 +32,10 @@ roteador.get('/:idFornecedor', async (req, res) => {
       id: idFornecedor
     });
     await fornecedor.carregar();
+    res.status(200);
     res.send(JSON.stringify(fornecedor))
   } catch(err){
+    res.status(404);
     res.send(
       JSON.stringify({
         msg:err.message
@@ -35,6 +44,7 @@ roteador.get('/:idFornecedor', async (req, res) => {
   }
 })
 
+// Update a Provider
 roteador.put('/:idFornecedor', async (req, res)=>{
   try {
     const {idFornecedor} = req.params;
@@ -42,23 +52,28 @@ roteador.put('/:idFornecedor', async (req, res)=>{
     const dados = Object.assign({}, dadosRecebidos, {id: idFornecedor});
     const fornecedor = new Fornecedor(dados);
     await fornecedor.atualizar();
+    res.status(204);
     res.end();
   }
   catch(err){
+    res.status(400);
     res.send(JSON.stringify({
       msg: err.message
     }));
   }
 })
 
+//Delete a Provider
 roteador.delete('/:idFornecedor', async (req, res)=>{
   try{
     const {idFornecedor} = req.params;
     const fornecedor = new Fornecedor({id:idFornecedor});
     await fornecedor.carregar();
     await fornecedor.remover();
+    res.status(204);
     res.end();
   } catch(err){
+    res.status(404);
     res.send(JSON.stringify({msg:err.message}));
   }
   
